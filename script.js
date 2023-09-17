0b1080d9f949ffb5b0fd9b62d768551ad53756c1
hamdi zaghouan <141347852+hamdiz0@users.noreply.github.com>
2023-09-17 18:42:54 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const audio1 = new Audio("btn.mp3")
+const audio2 = new Audio("pointg.mp3")
+const audio3 = new Audio("ball.mp3")
+const audio4 = new Audio("br.mp3")
+
 p = document.querySelector('.p')
 p1 = document.querySelector('.p1')
 p2 = document.querySelector('.p2')
@@ -12,6 +17,7 @@ function up1(){
     }else{
         p1.style.top = 0 + '%' ;
     }
+    audio1.play();
 }
 function down1(){
     if (topv != 80 ){
@@ -19,7 +25,8 @@ function down1(){
         p1.style.top = topv + '%' ;
     }else{
         p1.style.bottom = 0 + '%' ;
-    }     
+    }
+    audio1.play();     
 }
 function up2(){
     if (topv2 != 0 ){
@@ -28,6 +35,7 @@ function up2(){
     }else{
         p2.style.top = 0 + '%' ;
     }
+    audio1.play();
 }
 function down2(){
     if (topv2 != 80 ){
@@ -36,6 +44,7 @@ function down2(){
     }else{
         p2.style.bottom = 0 + '%' ;
     }  
+    audio1.play();
 }
 document.addEventListener('keydown' , (e)=>{
     if (e.key == "z"){up1()}
@@ -76,40 +85,55 @@ function move (){
         ballRect.left <= p1r.right &&
         ballRect.bottom >= p1r.top &&
         ballRect.top <= p1r.bottom
-    ) {velx = -velx;}
+    ) {velx = -velx;audio3.play();}
     if (
         ballRect.right >= p2r.left &&
         ballRect.left <= p2r.right &&
         ballRect.bottom >= p2r.top &&
         ballRect.top <= p2r.bottom
-      ) {velx = -velx;}
+      ) {velx = -velx;audio3.play();}
     
     if (x==0){
         sc1=sc1+1
         s2.textContent = sc1
         x = 50
         y = 50
+        audio2.play();
     }
     if (x==maxw){
         sc2=sc2+1
         s1.textContent = sc2
         x = 50
         y = 50
+        audio2.play();
     }
 }
 let speed = 16
 let interval
-interval = setInterval(move,speed)
-
 function spup(){
     speed = speed - 5
     clearInterval(interval);
     interval = setInterval(move,speed);
+    audio1.play();
 }
-
 function spdown(){
     speed = speed + 5
     clearInterval(interval);
     interval = setInterval(move,speed);
+    audio1.play();
+}
+function br(){
+    audio4.play()
+    audio4.loop = true
+    audio4.volume= 0.1;
+}
+function start(){
+    interval = setInterval(move,speed);
+    audio1.play();
+    br();
+}
+function pause(){
+    clearInterval(interval);
+    audio1.play();
 }
 
